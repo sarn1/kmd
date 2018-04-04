1996afb26ec3796c2babebae6ff9f3c90a2d7307
sarn1 <sarn@tyndale.com>
2018-04-03 21:01:44 -0500

    menu + hr

--- header.php
+++ header.php
@@ -18,6 +18,45 @@
   <div class="subtitle">The Kristyna M. Driehaus Foundation</div>
 
   <nav>
-      <?php wp_nav_menu(array('theme_location'=>'main-menu','container_class'=>'main-menu')); ?>
+    <ul>
+    <?php // wp_nav_menu(array('theme_location'=>'main-menu','container_class'=>'main-menu')); ?>
+    <!-- custom menu -->
+    <?php
+
+    global $wp_query;
+    $args = array(
+        'post_type' => 'menu-item',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+    );
+    $wp_query = new WP_Query( $args );
+
+    if ( $wp_query->have_posts() ) {
+        while ( $wp_query->have_posts() ) {
+            $wp_query->the_post();
+            //$portrait = get_the_post_thumbnail(get_the_ID(),'full',array('class' =>  ' portrait-img-small img-responsive'));
+            $url = get_post_meta(get_the_ID(), 'wpcf-url', true);
+            $img_on = get_post_meta(get_the_ID(), 'wpcf-image-on', true);
+            $img_off = get_post_meta(get_the_ID(), 'wpcf-image-off', true);
+            $title = get_the_title();
+    ?>
+      <li>
+          <a href="<?=$url?>">
+            <img src="<?=$img_off?>" title="<?=$title?>" alt="<?$title?>" onmouseover="this.src='<?=$img_on?>'" onmouseout="this.src='<?=$img_off?>'" />
+          </a>
+          <span class="title"><?=$title?></span>
+      </li>
+    <?php
+
+        }
+    }
+
+    wp_reset_query();
+    wp_reset_postdata();
+
+    ?>
+    </ul>
   </nav>
 </header>
+
+
--- index.php
+++ index.php
@@ -28,4 +28,5 @@
   endif;
   ?>
 </div>
+
 <?php get_footer(); ?>
